a70099eaf9fb622e50b603ea29d8f6b94d173cea
Skyleter <mogok1penguin@gmail.com>
2016-03-20 18:22:31 -0300

    Create skins.js

---
+++ skins.js
@@ -0,0 +1,11 @@
+image = new Image();
+image.crossOrigin = 'anonymous';
+image.src = 'i.imgur.com/p1gSxrH.png';
+window.agar.hooks.cellSkin = function(cell, old_skin) {
+	var skins1 = document.getElementById
+    if (cell.name == 'test') return image;
+    return old_skin;
+}    
+
+
+
