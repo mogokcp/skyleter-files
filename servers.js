612e9674a9440d84956aa13b458df4e65a4dda92
Skyleter <mogok1penguin@gmail.com>
2016-03-20 00:10:05 -0300

    Create servers.js

---
+++ servers.js
@@ -0,0 +1,10 @@
+[
+	{	
+		"type":"info",
+		"ip":"vps62061.vps.ovh.ca:443",
+		"gamemode":"Juggernaut Mode",
+		"owner":"IllIlIllIIIlIl",
+		"region":"NA",
+		"info":"https://redd.it/3x09gd"
+	} 
+]
