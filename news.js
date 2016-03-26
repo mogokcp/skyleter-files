69a8e965d97d36dc23817ddb9eb33510f9b3a911
Skyleter <mogok1penguin@gmail.com>
2016-03-26 01:05:53 -0300

    Update news.js

--- news.js
+++ news.js
@@ -1,6 +1,6 @@
 [
 	{	
 		"color":"color: red;",
-		"info":"<b>Hello there! welcome to octbots 2.2!, some of the changes are: <br>- UUID is now at the canvas, if you dont see it try refreshing the page.<br>- By default, the chat is now hidden.</b><br>"
+		"info":"<b>Hello there! welcome to octbots 2.2!, some of the changes are: <br>- UUID is now at the canvas, if you dont see it try refreshing the page.<br>- By default, the chat is now hidden.</b>"
 	}
 ]
