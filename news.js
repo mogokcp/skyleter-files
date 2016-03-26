f98cbe246fcf010049ee51b332ea3558d09a3f86
Skyleter <mogok1penguin@gmail.com>
2016-03-26 00:54:27 -0300

    Update news.js

--- news.js
+++ news.js
@@ -1,6 +1,6 @@
 [
 	{	
 		"color":"color: red;",
-		"info":"Hello there! welcome to octbots 2.2! <br>Userscript is now at the canvas, if you dont see it try refreshing the page."
+		"info":"Hello there! welcome to octbots 2.2! <br>- Userscript is now at the canvas, if you dont see it try refreshing the page.<br>- By default, the chat is now hidden."
 	}
 ]
