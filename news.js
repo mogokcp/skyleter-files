8576a730596c5c08c20f48033df09556d00ad6a4
Skyleter <mogok1penguin@gmail.com>
2016-03-27 02:27:29 -0300

    Update news.js

--- news.js
+++ news.js
@@ -1,6 +1,6 @@
 [
 	{	
 		"color":"color: red;",
-		"info":"<b>Hello there! welcome to octbots 2.2!, some of the changes are: <br>- UUID is now at the canvas, if you dont see it try refreshing the page.<br>- By default, the chat is now hidden.<br>-FEED ULTRA FAST (MACROFEED) WITH Q! <br>- Many other features! ;)</b>"
+		"info":"<b>Hello there! welcome to octbots 2.2!, some of the changes are: <br>- UUID is now at the canvas, if you dont see it try refreshing the page.<br>- By default, the chat is now hidden.<br>-FEED ULTRA FAST (MACROFEED) WITH Q! <br>- Many other features! ;)<br>**IF BOTS NOT WORKING MAKE SURE YOU EDITED THE SERVER IP** (default : ogar-feeder one).</b>"
 	}
 ]
